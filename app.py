b5ee5bd4f7ab458b7ef43dd38b52b580ce067422
nicha9 <nichanan2000s@gmail.com>
2022-11-24 17:53:51 +0700

    install venv use import

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 from flask import Flask, request
-#import json
+import json
 # import pyrebase  #pip install pyrebase4
 
-#import numpy as np
-#import cv2
+import numpy as np
+import cv2
 
-#from code_pattern1 import process
+from code_pattern1 import process
 
 
 app = Flask(__name__)
 
-# @app.route("/processing", methods=["POST"])
-# def index():
-#     # Image
-#     imagefile = request.files["image"].read()
+@app.route("/processing", methods=["POST"])
+def index():
+    # Image
+    imagefile = request.files["image"].read()
 
 
-#     npimg = np.fromstring(imagefile, np.uint8)
-#     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-#     # image = Image.fromarray(img)
+    npimg = np.fromstring(imagefile, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    # image = Image.fromarray(img)
     
 
-#     error, x_point = process(img)
-#     print("point"+ str(x_point))
+    error, x_point = process(img)
+    print("point"+ str(x_point))
 
 
-#     return json.dumps({"error_img": error.tolist(), "score": x_point})
+    return json.dumps({"error_img": error.tolist(), "score": x_point})
 
 @app.route('/')
 def home():
