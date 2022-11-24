5c90641d742eaa2da30f2bd330599573f2887883
nicha9 <nichanan2000s@gmail.com>
2022-11-24 17:41:00 +0700

    test

--- app.py
+++ app.py
@@ -10,22 +10,22 @@ from code_pattern1 import process
 
 app = Flask(__name__)
 
-@app.route("/processing", methods=["POST"])
-def index():
-    # Image
-    imagefile = request.files["image"].read()
+# @app.route("/processing", methods=["POST"])
+# def index():
+#     # Image
+#     imagefile = request.files["image"].read()
 
 
-    npimg = np.fromstring(imagefile, np.uint8)
-    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    # image = Image.fromarray(img)
+#     npimg = np.fromstring(imagefile, np.uint8)
+#     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+#     # image = Image.fromarray(img)
     
 
-    error, x_point = process(img)
-    print("point"+ str(x_point))
+#     error, x_point = process(img)
+#     print("point"+ str(x_point))
 
 
-    return json.dumps({"error_img": error.tolist(), "score": x_point})
+#     return json.dumps({"error_img": error.tolist(), "score": x_point})
 
 @app.route('/')
 def home():
